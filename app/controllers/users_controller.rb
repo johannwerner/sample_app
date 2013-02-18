329aa833c7d0a1a2a126c5dedc9fd4d0dd8f72ad
Johann <johann@immedia.co.za>
2013-02-18 16:17:58 +0200

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-
+before_filter :signed_in_user, only: [:index, :edit, :update, :destroy]
+before_filter :correct_user,   only: [:edit, :update]
+before_filter :admin_user,     only: :destroy
   def index
 		@user = User.all
   end
@@ -16,6 +18,30 @@ class UsersController < ApplicationController
   	@user = User.new  	
   end
 
+  def edit
+    #@user = User.find(params[:id])    
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+ def update
+    #@user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:sucess] = "Profile updated"
+      sign_in @user
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    redirect_to users_url
+  end
+
   def create
     @user = User.new(params[:user])
     if @user.save
@@ -26,4 +52,21 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+private
+
+    def signed_in_user
+      unless  signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(current_user) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
 end
